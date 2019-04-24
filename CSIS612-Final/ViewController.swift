a2a34505d882498fc24cd1d951c8dfafec5cb266
Hassam Solano-Morel <HassamSolano@gmail.com>
2019-04-23 20:23:55 -0400

    Initial Commit

---
+++ CSIS612-Final/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CSIS612-Final
+//
+//  Created by Hassam Solano-Morel on 4/23/19.
+//  Copyright © 2019 Hassam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
